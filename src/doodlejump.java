5b947021efeb319ddd1d1ebac55d6d9e6b2b7385
nicki <nicki.faustino@stud.fh-campuswien.ac.at>
2021-12-23 10:31:19 +0100

    Nicki Test

--- src/doodlejump.java
+++ src/doodlejump.java
@@ -1,6 +1,11 @@
 public class doodlejump {
+
     public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
         // Ausgabe Hello World!
         System.out.println("Hello World!");
+        int c = x+y;
+        System.out.println("x+y= "+c);
     }
 }
